b2c6eabf005ee95be99535a713345c052f325379
ivan-dev-flx <ivantoktraff1@gmail.com>
2025-07-29 19:07:23 +0200

    Update vip.dart

--- lib/src/core/models/vip.dart
+++ lib/src/core/models/vip.dart
@@ -1,13 +1,42 @@
-import 'package:purchases_flutter/purchases_flutter.dart';
-
 class Vip {
-  Vip({
+  final bool isVip;
+  final bool loading;
+  final dynamic offering; // Замените на правильный тип (например, Offering?)
+
+  const Vip({
     this.isVip = false,
     this.loading = false,
     this.offering,
   });
 
-  final bool isVip;
-  final bool loading;
-  final Offering? offering;
+  Vip copyWith({
+    bool? isVip,
+    bool? loading,
+    dynamic offering,
+  }) {
+    return Vip(
+      isVip: isVip ?? this.isVip,
+      loading: loading ?? this.loading,
+      offering: offering ?? this.offering,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Vip(isVip: $isVip, loading: $loading, offering: $offering)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Vip &&
+        other.isVip == isVip &&
+        other.loading == loading &&
+        other.offering == offering;
+  }
+
+  @override
+  int get hashCode {
+    return isVip.hashCode ^ loading.hashCode ^ offering.hashCode;
+  }
 }
